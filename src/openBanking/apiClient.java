ecb87e39a99b4ff2f7ae29573be37740faf7a254
rmsgkr <rmsgkr2@gmail.com>
2020-11-17 17:35:23 +0900

    final version

---
+++ src/openBanking/apiClient.java
@@ -0,0 +1,20 @@
+package openBanking;
+
+public class apiClient {
+	private static String client_id = "bj58uxKmufhcOS8NZoObuBiYh1II0Bcl1NCGfN26";
+	private static String client_secret = "edWLkSwIuI8uYMRaGEIBiPlzC2oDH6CcRvjNUzcj";
+	private static String client_use_code = "T991648480";
+
+	public static String getId() {
+		return client_id;
+	}
+
+	public static String getSecret() {
+		return client_secret;
+	}
+
+	public static String getCode() {
+		return client_use_code;
+	}
+
+}
